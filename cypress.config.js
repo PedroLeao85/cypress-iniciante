ab980b93191a35c60043586ac7ddbbbb75cd2e1c
PedroLeao85 <phalves1@hotmail.com>
2025-11-06 15:58:30 -0300

    Configura BaseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl : 'https://automationpratice.com.br/',
+     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
