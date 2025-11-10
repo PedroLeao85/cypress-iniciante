d47deea67d3f120c6a538380682d6f1da060318e
PedroLeao85 <phalves1@hotmail.com>
2025-11-10 17:45:56 -0300

    Captura um texto / element

--- cypress/e2e/get_texts.cy.js
+++ cypress/e2e/get_texts.cy.js
@@ -1,29 +1,18 @@
-describe('Login', () => {
+describe('Get Texts', () => {
 
-    it('Login com sucesso', () => {
-        cy.visit('/') // dessa maneira acessa a baseUrl configurada no cypress.config.js
+    it('Obter texto de um elemento', () => {
+        cy.visit('/')
             .get('.header-logo')
 
-        cy.get('#top_header').as('cabecalho')
-            .contains('Login')
-            .click()    
+        cy.get('.top_header_left > p')
+            .then((element) => {
+                console.log(element.text())
+            })    
 
-        cy.get('#top_header')
-           .find('.fa-user')
-           
-        cy.get('@cabecalho')
-            .find('.fa-user')
+        
     })
  
 
 })
 
-// get() - seleciona elementos
-
-//contains() - seleciona elementos com base no texto
-//geralmente diminuimos o escopo com um get()
-
-// find() - para encontrar elementos
-// geralmente diminuimos o escopo com um get()
-
-// as() - para criar um alias (apelido) para um elemento
+// then
