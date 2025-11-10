133797851ddc21fccf1031ebeafa3cb365a16873
PedroLeao85 <phalves1@hotmail.com>
2025-11-10 16:59:32 -0300

    Cria get, find, contains, as(alias)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,14 +2,27 @@ describe('Login', () => {
 
     it('Login com sucesso', () => {
         cy.visit('/') // dessa maneira acessa a baseUrl configurada no cypress.config.js
+            .get('.header-logo')
+
+        cy.get('#top_header').as('cabecalho')
+            .contains('Login')    
+
+        cy.get('#top_header')
+           .find('.fa-user')
+           
+        cy.get('@cabecalho')
+            .find('.fa-user')
     })
-
-    it('Email com sucesso', () => {
-
-    })
-
-    it('Senha inválida', () => {
-
-    })
+ 
 
 })
+
+// get() - seleciona elementos
+
+//contains() - seleciona elementos com base no texto
+//geralmente diminuimos o escopo com um get()
+
+// find() - para encontrar elementos
+// geralmente diminuimos o escopo com um get()
+
+// as() - para criar um alias (apelido) para um elemento
