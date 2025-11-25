2cff2444913893580dd6e4c04c116a7e6334b612
PedroLeao85 <phalves1@hotmail.com>
2025-11-24 22:31:24 -0300

    Adiciona expect

--- cypress/e2e/asserts.cy.js
+++ cypress/e2e/asserts.cy.js
@@ -1,15 +1,28 @@
 /// <reference types="cypress" />
 describe('Asserts', () => {
 
-    it('Verificar se esta visivel', () => {
+    it.only('Verificar se esta visivel', () => {
         cy.visit('/')
             .get('.header-logo')
 
-        cy.get('.fa-user').click()
+        cy.get('.fa-user')
+            .should('be.visible')
+            .click()
+
         cy.get('.account_form > h3').should('be.visible')
+
         cy.get('.account_form > h3')
             .should('contain', 'Login')
             .should('have.text', 'Login')
+
+        cy.get('.account_form > h3')
+            .then((element) => {
+                expect(element.text()).equal('Login')
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })    
+
+
     })
 
    
