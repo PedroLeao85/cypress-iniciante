35fff0c780ba9962ec2ac106800358519b21f6e2
PedroLeao85 <phalves1@hotmail.com>
2025-12-02 19:52:20 -0300

    Altera desafio.cy.js

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -3,79 +3,175 @@
 describe('Cadastro de usuário', () =>{
 
     it('Validar campo nome vazio', () => {
+        // cy.visit('/')
+        //     .get('.header-logo')
+        // cy.get('.fa-lock').click()
+        // cy.get('#user').click()
+        // cy.get('#email').click().type('pedro.leao@123.com.br')
+        // cy.get('#password').click().type('123456')
+        // cy.get('#btnRegister').click()
+        // cy.get('#errorMessageFirstName')
+        //     .should('contain', 'O campo nome deve ser prenchido').and('be.visible')
+        
         cy.visit('/')
-        cy.get('.fa-lock').click()
-        cy.get('#user').click()
-        cy.get('#email').click().type('pedro.leao@123.com.br')
-        cy.get('#password').click().type('123456')
-        cy.get('#btnRegister').click()
-        cy.get('#errorMessageFirstName')
-            .should('contain', 'O campo nome deve ser prenchido').and('be.visible')
+             .get('.header-logo')
+        
+        cy.get('.fa-lock')
+            .click()
+            .get('#user')
+            .should('be.visible')     
+
+        cy.get('#btnRegister')
+            .click()
+            
+        cy.get('.errorLabel')
+            .should('have.text', 'O campo nome deve ser prenchido')    
     })
 
     
     it('Validar campo e-mail vazio', () => {
+        // cy.visit('/')
+        // cy.get('.fa-lock').click()
+        // cy.get('#user').click().type('Pedro')
+        // cy.get('#email').click()
+        // cy.get('#password').click().type('123456')
+        // cy.get('#btnRegister').click()
+        // cy.get('#errorMessageFirstName')
+        //     .should('contain', 'O campo e-mail deve ser prenchido corretamente')
+        //     .and('be.visible')
+
         cy.visit('/')
-        cy.get('.fa-lock').click()
-        cy.get('#user').click().type('Pedro')
-        cy.get('#email').click()
-        cy.get('#password').click().type('123456')
-        cy.get('#btnRegister').click()
-        cy.get('#errorMessageFirstName')
-            .should('contain', 'O campo e-mail deve ser prenchido corretamente')
-            .and('be.visible')
+             .get('.header-logo')
+        
+        cy.get('.fa-lock')
+            .click()
+            .get('#user').click().type('Pedro')
+            .get('#password').click().type('123456')
+            .should('be.visible')     
+
+        cy.get('#btnRegister')
+            .click()
+            
+        cy.get('.errorLabel')
+            .should('have.text', 'O campo e-mail deve ser prenchido corretamente') 
         
     })
 
     
     it('Validar campo e-mail inválido', () => {
+        // cy.visit('/')
+        // cy.get('.fa-lock').click()
+        // cy.get('#user').click().type('Pedro')
+        // cy.get('#email').click().type('Pedro')
+        // cy.get('#password').click().type('123456')
+        // cy.get('#btnRegister').click()
+        // cy.get('#errorMessageFirstName')
+        //     .should('contain', 'O campo e-mail deve ser prenchido corretamente')
+        //     .and('be.visible')
+        
         cy.visit('/')
-        cy.get('.fa-lock').click()
-        cy.get('#user').click().type('Pedro')
-        cy.get('#email').click().type('Pedro')
-        cy.get('#password').click().type('123456')
-        cy.get('#btnRegister').click()
-        cy.get('#errorMessageFirstName')
-            .should('contain', 'O campo e-mail deve ser prenchido corretamente')
-            .and('be.visible')
+             .get('.header-logo')
+        
+        cy.get('.fa-lock')
+            .click()
+            .get('#user').click().type('Pedro')
+            .get('#email').click().type('Pedro')
+            .get('#password').click().type('123456')
+            .should('be.visible')     
+
+        cy.get('#btnRegister')
+            .click()
+            
+        cy.get('.errorLabel')
+            .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
+
     })
 
     
     it('Validar campo senha vazio', () => {
+        // cy.visit('/')
+        // cy.get('.fa-lock').click()
+        // cy.get('#user').click().type('Pedro')
+        // cy.get('#email').click().type('pedro@123.com')
+        // cy.get('#password').click()
+        // cy.get('#btnRegister').click()
+        // cy.get('#errorMessageFirstName')
+        //     .should('contain', 'O campo senha deve ter pelo menos 6 dígitos')
+        //     .and('be.visible')
+
         cy.visit('/')
-        cy.get('.fa-lock').click()
-        cy.get('#user').click().type('Pedro')
-        cy.get('#email').click().type('pedro@123.com')
-        cy.get('#password').click()
-        cy.get('#btnRegister').click()
-        cy.get('#errorMessageFirstName')
-            .should('contain', 'O campo senha deve ter pelo menos 6 dígitos')
-            .and('be.visible')
+             .get('.header-logo')
+        
+        cy.get('.fa-lock')
+            .click()
+            .get('#user').click().type('Pedro')
+            .get('#email').click().type('pedro@123.com')
+            .should('be.visible')     
+
+        cy.get('#btnRegister')
+            .click()
+
+        cy.get('.errorLabel')
+            .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')    
     })
 
     it('Validar campo senha inválida', () => {
+        // cy.visit('/')
+        // cy.get('.fa-lock').click()
+        // cy.get('#user').click().type('Pedro')
+        // cy.get('#email').click().type('pedro@123.com')
+        // cy.get('#password').click().type('123')
+        // cy.get('#btnRegister').click()
+        // cy.get('#errorMessageFirstName')
+        //     .should('contain', 'O campo senha deve ter pelo menos 6 dígitos')
+        //     .and('be.visible')
         cy.visit('/')
-        cy.get('.fa-lock').click()
-        cy.get('#user').click().type('Pedro')
-        cy.get('#email').click().type('pedro@123.com')
-        cy.get('#password').click().type('123')
-        cy.get('#btnRegister').click()
-        cy.get('#errorMessageFirstName')
-            .should('contain', 'O campo senha deve ter pelo menos 6 dígitos')
-            .and('be.visible')
+             .get('.header-logo')
+        
+        cy.get('.fa-lock')
+            .click()
+            .get('#user').click().type('Pedro')
+            .get('#email').click().type('pedro@123.com')
+            .get('#password').click().type('123')
+            .should('be.visible')     
+
+        cy.get('#btnRegister')
+            .click()
+
+        cy.get('.errorLabel')
+            .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
+
     })
 
-    it('Cadastro realizado com sucesso', () => {
+    it.only('Cadastro realizado com sucesso', () => {
+        // cy.visit('/')
+        // cy.get('.fa-lock').click()
+        // cy.get('#user').click().type('Pedro')
+        // cy.get('#email').click().type('pedro@123.com')
+        // cy.get('#password').click().type('123456')
+        // cy.get('#btnRegister').click()
+        // cy.get('#swal2-title')
+        //     .should('contain', 'Cadastro realizado!')
+        // cy.get('#swal2-html-container')
+        //     .should('contain', 'Bem-vindo Pedro')    
+
         cy.visit('/')
-        cy.get('.fa-lock').click()
-        cy.get('#user').click().type('Pedro')
-        cy.get('#email').click().type('pedro@123.com')
-        cy.get('#password').click().type('123456')
-        cy.get('#btnRegister').click()
+             .get('.header-logo')
+        
+        cy.get('.fa-lock')
+            .click()
+            .get('#user').click().type('Pedro')
+            .get('#email').click().type('pedro@123.com')
+            .get('#password').click().type('123456')
+            .should('be.visible')     
+
+        cy.get('#btnRegister')
+            .click()
+
         cy.get('#swal2-title')
-            .should('contain', 'Cadastro realizado!')
-        cy.get('#swal2-html-container')
-            .should('contain', 'Bem-vindo Pedro')    
+            .should('contain', 'Cadastro realizado!')      
+            .get('#swal2-html-container')
+            .should('contain', 'Bem-vindo Pedro')
     })
     
     
